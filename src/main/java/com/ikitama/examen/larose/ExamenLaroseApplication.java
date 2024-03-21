de2a68f99a1ffa6c1980bdcea42a431d61896a8e
Larose <imdev242@gmail.com>
2024-03-21 14:13:43 +0000

    Premier commit

---
+++ src/main/java/com/ikitama/examen/larose/ExamenLaroseApplication.java
@@ -0,0 +1,13 @@
+package com.ikitama.examen.larose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenLaroseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenLaroseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikitama/examen/larose/ExamenLaroseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikitama.examen.larose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenLaroseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
