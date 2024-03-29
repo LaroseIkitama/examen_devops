f378a8f76aa38c38ae4b0775e48feb0fa2fa2a82
Larose <imdev242@gmail.com>
2024-03-29 03:31:10 +0000

    nouveau commit

--- src/test/java/com/ikitama/examen/larose/ProductControllerTest.java
+++ src/test/java/com/ikitama/examen/larose/ProductControllerTest.java
@@ -18,7 +18,6 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
 public class ProductControllerTest {
 
     @Mock
